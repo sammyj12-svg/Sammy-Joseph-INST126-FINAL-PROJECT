dd5e15eadd7491dcb71d6dfba7e55a3d67010fb9
sammyj12-svg <sammyj12@terpmail.umd.edu>
2026-05-13 01:32:12 -0400

    Finalizing turn()
    Created if statements for all scenarios in turn(), created if statement for user to stop re-rolling and print the sum of dice, moved roll_dice() out of while loop in turn() due to realizing the error it caused

--- scripts.py
+++ scripts.py
@@ -7,25 +7,40 @@ def roll_dice():
 
 
 def turn():
+    dice = roll_dice()
+    print("Start roll:", dice)
     continue_turn = True 
     while continue_turn:
-        dice = roll_dice()
-        print("Start roll:", dice)
+        
         if dice [0] == dice [1] == dice [2]:
             print("tuple out!: 0 points")
             return 0
         
         if dice[0] == dice [1]:
             again = input ("Dice 1 and Dice 2 are fixed, would you like to roll dice 3 again? (y/n): ")
+            if again == "y"
+                dice[2] = np.random.randint(1,7)
+                print(dice[0], dice[1], dice[2])
     
         elif dice[1] == dice [2]:
             again = input ("Dice 2 and Dice 3 are fixed would you like to roll dice 1 again? (y/n): ")
+            if again == "y"
+                dice[0] = np.random.randint(1,7)
+                print(dice[0], dice[1], dice[2])
 
         elif dice[0] == dice [2]:
             again = input ("Dice 1 and Dice 3 are fixed, would you like to roll dice 2 again? (y/n): ")
+            if again == "y"
+                dice[1] = np.random.randint(1,7)
+                print(dice[0], dice[1], dice[2])
 
         else: 
             again = input("No matching dice, would you like to roll again? (y/n): ")
+            if again == "y":
+                continue_turn
+        if again == "n":
+            continue_turn = False
+            print (sum(dice))
     return sum(dice)
 
     
