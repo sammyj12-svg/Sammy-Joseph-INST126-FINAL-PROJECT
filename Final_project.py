9431805b595f8af13b4762e28f8dec1a0d4dc11c
sammyj12-svg <sammyj12@terpmail.umd.edu>
2026-05-13 10:49:40 -0400

    Adding player decided win_condition
    I am adding an opening while true input loop so they players can decide what score they want to play to. Though there is an error when the player enters something that is not an integer

--- Final_project.py
+++ Final_project.py
@@ -4,6 +4,16 @@ import scripts
 player1_score = 0
 player2_score = 0
 
+while True:
+    win_condition = int(input("What score do you want to play to? (30-50)"))
+    if win_condition != int:
+        print ("Please enter a numer")
+        continue
+    if win_condition > 50 or win_condition < 30:
+        print("Please enter a number between (30-50)")
+    else: 
+        break
+
 while player1_score < 50 and player2_score < 50:
     print ("Player 1 turn:")
     turn_score = scripts.turn()    
