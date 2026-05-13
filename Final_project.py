0005c846444b46982bf593bed1296ebd12613971
sammyj12-svg <sammyj12@terpmail.umd.edu>
2026-05-13 02:15:22 -0400

    fix game loop logic and clean up turn output messages
    Switched if statements so losing player would not get extra turn, made prints more descriptive, removed uncessary roll_dice in main file

--- Final_project.py
+++ Final_project.py
@@ -5,18 +5,22 @@ player1_score = 0
 player2_score = 0
 
 while player1_score < 50 and player2_score < 50:
-    scripts.roll_dice()
+    print ("Player 1 turn:")
     turn_score = scripts.turn()    
     player1_score += turn_score
-
-    scripts.roll_dice()
-    turn_score = scripts.turn()    
-    player2_score += turn_score
+    print("Player 1's current score is:", player1_score)
 
     if player1_score >= 50:
-        print("player 1 wins")
+        print("game over, player 1 wins!!!")
+
+    print ("Player 2 turn:")
+    turn_score = scripts.turn()    
+    player2_score += turn_score
+    print("Player 2's current score is:", player2_score)
+
+
     if player2_score >= 50: 
-        print ("player 2 wins")
+        print("game over, player 2 wins!!!")
 
 
 
