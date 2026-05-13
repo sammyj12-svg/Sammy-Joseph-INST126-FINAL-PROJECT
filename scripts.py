b098a356856f8de71e961f2486afacac82856873
sammyj12-svg <sammyj12@terpmail.umd.edu>
2026-05-13 00:55:52 -0400

    Finishing turn()
    Created a while loop inside turn(), added the if, elif, and else statements previously in main file. Have turn() return sum of dice

--- scripts.py
+++ scripts.py
@@ -7,10 +7,25 @@ def roll_dice():
 
 
 def turn():
-    dice = roll_dice()
-    print("Start roll:", dice)
-    if dice [0] == dice [1] == dice [2]:
-        print("tuple out!: 0 points")
-        return 0
+    continue_turn = True 
+    while continue_turn:
+        dice = roll_dice()
+        print("Start roll:", dice)
+        if dice [0] == dice [1] == dice [2]:
+            print("tuple out!: 0 points")
+            return 0
+        
+        if dice[0] == dice [1]:
+            again = input ("Dice 1 and Dice 2 are fixed, would you like to roll dice 3 again? (y/n): ")
+    
+        elif dice[1] == dice [2]:
+            again = input ("Dice 2 and Dice 3 are fixed would you like to roll dice 1 again? (y/n): ")
+
+        elif dice[0] == dice [2]:
+            again = input ("Dice 1 and Dice 3 are fixed, would you like to roll dice 2 again? (y/n): ")
+
+        else: 
+            again = input("No matching dice, would you like to roll again? (y/n): ")
+    return sum(dice)
 
     
