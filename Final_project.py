96929b9eeb4af4bc15e4e469ae8217b3ddf7ff9b
sammyj12-svg <sammyj12@terpmail.umd.edu>
2026-05-13 12:11:16 -0400

    Comments and docstrings
    Added docstrings to functions and comments to all code to make it more readabale and understandable

--- Final_project.py
+++ Final_project.py
@@ -1,39 +1,50 @@
+#Imports numpy and seperate module 
 import numpy as np 
 import scripts
-
+#Assigns players scores at beginning of game
 player1_score = 0
 player2_score = 0
-
+#Parameters loop 
 while True:
-    try:
-        win_condition = int(input("What score do you want to play to? (30-50)"))
 
+    try:
+        #Asks the user what score they want to play to and allows the user to enter only an integer
+        win_condition = int(input("What score do you want to play to? (30-50)"))
+        #Makes sure the user enters a number within the range
         if win_condition > 50 or win_condition < 30:
             print("Please enter a number between (30-50)")
-        
+        #Breaks out of the parameters loop once win_condition is entered
         else: 
             break
-
+    # Catches an error if the user enters something that is not a number
     except ValueError: 
         print ("Please enter a numer")
 
     
-
+#Game loop 
 while player1_score < 50 and player2_score < 50:
+   
+    #Player 1 turn: 
     print ("Player 1 turn:")
-    turn_score = scripts.turn()    
+    #Calls the turn function and assigns it to turn_score 
+    turn_score = scripts.turn()  
+    #Adds the turn_score to player1_score  
     player1_score += turn_score
     print("Player 1's current score is:", player1_score)
-
+    
+    #win_condition for player 1
     if player1_score >= 50:
         print("game over, player 1 wins!!!")
 
+    #Player 2 turn: 
     print ("Player 2 turn:")
-    turn_score = scripts.turn()    
+    #Calls the turn function and assigns it to turn_score 
+    turn_score = scripts.turn()   
+    #Adds the turn_score to player2_score  
     player2_score += turn_score
     print("Player 2's current score is:", player2_score)
 
-
+    #win_condition for player 2
     if player2_score >= 50: 
         print("game over, player 2 wins!!!")
 
--- scripts.py
+++ scripts.py
@@ -1,21 +1,40 @@
 import numpy as np 
 def roll_dice():
+    """
+    Rolls three six_sided dice and returns their values 
+    (an array of random ints between 1-6) to user 
+    """
+    #Possible values for dice
     dice_array = np.array ([1, 2, 3, 4, 5, 6])
+    #randomly select 3 values, with replacement on 
     np.random.choice(dice_array, size = 3, replace = True)
+    #return said values 
     return np.random.choice(dice_array, size = 3, replace = True)
 
 
 
 def turn():
+    """
+    This function is for one player's turn during the game.
+    If all 3 dice match, player will tuple out and return 0.
+    If two dice match, they become fixed (if statements) and
+    cannot be rolled again after during the turn. 
+    Player can decicde to re-roll until they want to stop or tuple out. 
+    This function returns the sum of the dice values at the end of the turn. 
+    """
+    #Calls roll_dice at the start of the turn 
     dice = roll_dice()
     print("Start roll:", dice)
     continue_turn = True 
+
+    #Turn loop, keeps looping until player tuples out or decides to stop rolling
     while continue_turn:
         
+        #checks for tuple out
         if dice [0] == dice [1] == dice [2]:
             print("tuple out!: 0 points")
             return 0
-        
+        #fixed dice scenarios
         if dice[0] == dice [1]:
             again = input ("Dice 1 and Dice 2 are fixed, would you like to roll dice 3 again? (y/n): ")
             if again == "y"
@@ -33,12 +52,14 @@ def turn():
             if again == "y"
                 dice[1] = np.random.randint(1,7)
                 print(dice[0], dice[1], dice[2])
-
+        
+        #Re-roll option 
         else: 
             again = input("No matching dice, would you like to roll again? (y/n): ")
             if again == "y":
                 dice = roll_dice()
                 print("Start roll:", dice)
+        #If the player chooses to stop rolling the loop will end and the dice results will be printed 
         if again == "n":
             continue_turn = False
             print (sum(dice))
