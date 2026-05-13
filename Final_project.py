a928a21cfd3259b8ea3683d5a501b8ae44935592
sammyj12-svg <sammyj12@terpmail.umd.edu>
2026-05-13 02:00:44 -0400

    Main file while loop
    I created variables to store both players scores. I also made a main game while loop which chekcs if either player has reached 50 points. Each turn will consist of rolling the dice, and the sum of the dice or turn_score will add to player1 or player2 score until 50 is reached

--- Final_project.py
+++ Final_project.py
@@ -1,8 +1,23 @@
 import numpy as np 
 import scripts
 
-scripts.roll_dice()
-scripts.turn()    
+player1_score = 0
+player2_score = 0
+
+while player1_score < 50 and player2_score < 50:
+    scripts.roll_dice()
+    turn_score = scripts.turn()    
+    player1_score += turn_score
+
+    scripts.roll_dice()
+    turn_score = scripts.turn()    
+    player2_score += turn_score
+
+    if player1_score >= 50:
+        print("player 1 wins")
+    if player2_score >= 50: 
+        print ("player 2 wins")
+
 
 
 
